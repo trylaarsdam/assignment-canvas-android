9a76b379ce6766e7ca45c7830b1b182c117566e2
Todd Rylaarsdam <todd@toddr.org>
2022-07-08 17:42:40 -0500

    removed

--- app/src/main/java/com/trylaarsdam/assignmentcanvas/data/FeedCategory.kt
+++
@@ -1,5 +0,0 @@
-package com.trylaarsdam.assignmentcanvas.data
-
-import androidx.compose.ui.graphics.vector.ImageVector
-
-data class FeedCategory(val name: String, var active: Boolean, val icon: ImageVector)
--- app/src/main/java/com/trylaarsdam/assignmentcanvas/data/TabItem.kt
+++
@@ -1,5 +0,0 @@
-package com.trylaarsdam.assignmentcanvas.data
-
-import androidx.compose.ui.graphics.vector.ImageVector
-
-public data class TabItem(val name: String, val icon: ImageVector)
