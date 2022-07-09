1c34f72204f168852d635d3121bb86f3c98c9100
Todd Rylaarsdam <todd@toddr.org>
2022-07-09 11:22:14 -0500

    updatyes

---
+++ app/src/main/java/com/trylaarsdam/assignmentcanvas/api/responses/APIAnnouncement.kt
@@ -0,0 +1,8 @@
+package com.trylaarsdam.assignmentcanvas.api.responses
+
+import com.trylaarsdam.assignmentcanvas.api.objects.Announcement
+
+data class APIAnnouncement(
+    val data: Announcement,
+    val status: String
+)
