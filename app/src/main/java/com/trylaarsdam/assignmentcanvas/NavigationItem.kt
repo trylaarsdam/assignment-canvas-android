36d927c8844901c13d460995ab0d64b2125cdcb7
Todd Rylaarsdam <todd@toddr.org>
2022-07-08 19:53:39 -0500

    updates

--- app/src/main/java/com/trylaarsdam/assignmentcanvas/NavigationItem.kt
+++ app/src/main/java/com/trylaarsdam/assignmentcanvas/NavigationItem.kt
@@ -1,11 +1,11 @@
 package com.trylaarsdam.assignmentcanvas
 
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Info
 import androidx.compose.material.icons.outlined.Menu
+import androidx.compose.material.icons.outlined.School
 import androidx.compose.ui.graphics.vector.ImageVector
 
 sealed class NavigationItem(var route: String, var icon: ImageVector, var title: String) {
     object Feed : NavigationItem("feed", Icons.Outlined.Menu, "Feed")
-    object Courses : NavigationItem("courses", Icons.Outlined.Info, "Feed")
+    object Courses : NavigationItem("courses", Icons.Outlined.School, "Courses")
 }
--- app/src/main/java/com/trylaarsdam/assignmentcanvas/ui/BottomNavigationBar.kt
+++ app/src/main/java/com/trylaarsdam/assignmentcanvas/ui/BottomNavigationBar.kt
@@ -3,7 +3,9 @@ package com.trylaarsdam.assignmentcanvas.ui
 import android.graphics.Color
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
 import com.trylaarsdam.assignmentcanvas.NavigationItem
 
 @Composable
@@ -16,14 +18,16 @@ fun BottomNavigationBar(navController: NavHostController) {
         backgroundColor = MaterialTheme.colors.background,
         contentColor = MaterialTheme.colors.onBackground
     ) {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
         items.forEach { item ->
             BottomNavigationItem(
                 icon = { Icon(item.icon, contentDescription = item.title) },
                 label = { Text(text = item.title) },
-                selectedContentColor = MaterialTheme.colors.onSecondary,
+                selectedContentColor = MaterialTheme.colors.primary,
                 unselectedContentColor = MaterialTheme.colors.onBackground,
                 alwaysShowLabel = true,
-                selected = false,
+                selected = currentRoute == item.route,
                 onClick = {
                     navController.navigate(item.route) {
                         // Pop up to the start destination of the graph to
