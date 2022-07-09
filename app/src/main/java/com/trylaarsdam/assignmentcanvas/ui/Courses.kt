02a9c0a2b932b21d4ad295d9a347d8a2ff1bf370
Todd Rylaarsdam <todd@toddr.org>
2022-07-08 21:31:30 -0500

    courses view loading

--- app/src/main/java/com/trylaarsdam/assignmentcanvas/ui/Courses.kt
+++ app/src/main/java/com/trylaarsdam/assignmentcanvas/ui/Courses.kt
@@ -13,6 +13,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.google.gson.Gson
 import com.trylaarsdam.assignmentcanvas.api.apiRequest
@@ -23,40 +24,44 @@ import kotlinx.coroutines.launch
 @Composable
 fun Courses(navController: NavController) {
     val coroutineScope = rememberCoroutineScope()
-
     var courses = remember {mutableStateOf(APICourses(listOf<Course>(), "loading"))}
-
-//    LazyColumn {
-//        items(courses) { course ->
-//            FeedCard(navController)
-//        }
-//    }
 
     LaunchedEffect(Unit) {
         coroutineScope.launch {
             courses.value = Gson().fromJson(apiRequest("api/courses/"), APICourses::class.java)
         }
     }
-    if(courses.value.status == "loading") {
-        Column(modifier = Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center) {
-            CircularProgressIndicator()
-        }
-    } else {
-        LazyColumn(modifier = Modifier
-            .padding(15.dp)
-            .fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
-            items(courses.value.data) { course ->
-                Card(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(5.dp)
-                        .clickable { },
-                    elevation = 10.dp
-                ) {
-                    Column(modifier = Modifier.fillMaxWidth().padding(10.dp)) {
-                        Text(course.name)
+    Column() {
+        Text(
+            "Courses",
+            modifier = Modifier.fillMaxWidth().padding(horizontal = 20.dp).padding(top = 20.dp),
+            fontSize = 30.sp
+        )
+        if (courses.value.status == "loading") {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                CircularProgressIndicator()
+            }
+        } else {
+            LazyColumn(
+                modifier = Modifier
+                    .padding(15.dp)
+                    .fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                items(courses.value.data) { course ->
+                    Card(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(5.dp)
+                            .clickable { },
+                        elevation = 10.dp
+                    ) {
+                        Column(modifier = Modifier.fillMaxWidth().padding(10.dp)) {
+                            Text(course.name)
+                        }
                     }
                 }
             }
